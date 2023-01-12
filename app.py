7c1a0156bba5661c39f37479f5d86aa45a683b0d
Nick Salvemini <salvemini.nick@gmail.com>
2023-01-12 13:37:43 -0500

    completed exercise

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import static.stories as stories
+from flask import Flask, render_template, request
+from stories import Story, story
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -8,10 +8,10 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home():
-    # questions = story.prompts
-    # return render_template('home.html', prompts=questions)
-    return story
+    questions = story.prompts
+    return render_template('home.html', prompts=questions)
 
 @app.route('/story')
-def story():
-    return render_template('story.html')
+def complete_story():
+    template = story.generate(request.args)
+    return render_template('story.html', template=template)
